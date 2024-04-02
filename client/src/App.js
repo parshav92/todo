85a100218ff32d0b4689a3e6f28c33c289ac9616
parshav92 <dedhiaparshav@gmail.com>
2024-04-02 22:18:14 +0530

    state uplift

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
-import TodoForm from "./components/todoForm";
-import ThemeSwitcher from "./components/ThemeSwitcher";
-import TodoTable from "./components/todoTable";
-import { Footer } from "./components/footer";
+import TodoContainer from "./components/todo";
+import ThemeSwitcher from "./components/common/ThemeSwitcher";
+import { Footer } from "./components/common/footer";
 
 function App() {
   return (
-    <div className="font-sans min-h-screen flex flex-col items-center justify-center bg-opacity-15 backdrop-blur-md mx-4">
-      <div
-        className="bg-opacity-50 rounded-lg p-4 w-full max-w-lg"
-        style={{ boxShadow: "0 0 16px 0 rgba(31, 38, 135, 0.3)" }}
-      >
-        <TodoForm />
-        <TodoTable />
-      </div>
+    <>
+      <TodoContainer />
       <div className="absolute top-4 right-4">
         <ThemeSwitcher />
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
